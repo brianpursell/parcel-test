6476928075f7414fb9477715ebe2e98b0e5f539b
brian pursell <brian.pursell@raise.com>
2019-03-08 17:22:55 -0600

    menu workin

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,18 @@
-const clickMe = document.getElementById("click-me");
+const menu = document.getElementById("menu");
+const menuButton = document.getElementById("menu-button");
 
-clickMe.onclick = () => alert("howdy");
+const close =
+  "M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z";
+const hamburger =
+  "M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z";
+
+menuButton.onclick = () => {
+  const { children } = menuButton;
+  const menuIconIsOpen = children[0].getAttribute("d") === hamburger;
+
+  menuIconIsOpen
+    ? children[0].setAttribute("d", close)
+    : children[0].setAttribute("d", hamburger);
+
+  menu.classList.toggle("show");
+};
